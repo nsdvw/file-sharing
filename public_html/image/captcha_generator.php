084bfc5d1d8f51257e36c10a13c835ec2f7dd05e
inside <inside@inside.com>
2015-08-22 19:21:24 +0300

    Добавлен генератор капчи

---
+++ public_html/image/captcha_generator.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: image/jpeg');
+session_start();
+
+$captcha = imagecreatefromjpeg('noise.jpg');
+$color = imagecolorallocate($captcha, 140, 140, 140);
+$letters = 5;
+$randomString = strtolower(substr(base64_encode(sha1(uniqid())), 0, $letters));
+$_SESSION['randomString'] = $randomString;
+
+$x = 20;
+$y = 55;
+$offset = 40;
+
+for ($i = 0; $i < $letters; $i++) {
+    $size = rand(30, 40);
+    $angle = rand(-25, 25);
+    imagettftext($captcha, $size, $angle, $x, $y, $color,
+        '../fonts/Chewy.ttf',
+        $randomString{$i});
+    $x += $offset;
+}
+imagejpeg($captcha);

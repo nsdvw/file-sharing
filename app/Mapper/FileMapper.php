b0dedddf846bc3b4f73a3cdaff39fa58ead7e831
inside <inside@inside.com>
2015-12-29 17:55:37 +0200

    Refactoring: use fetchColumn instead of fetch(PDO::FETCH_ASSOC)

--- app/Mapper/FileMapper.php
+++ app/Mapper/FileMapper.php
@@ -75,7 +75,6 @@ class FileMapper extends AbstractMapper
         $sql = "SELECT COUNT(*) as page_count FROM file";
         $sth = $this->connection->prepare($sql);
         $sth->execute();
-        $res = $sth->fetch(\PDO::FETCH_ASSOC);
-        return $res['page_count'];
+        return $sth->fetchColumn();
     }
 }

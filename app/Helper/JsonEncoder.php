c2fabdb8092222e956d3b8453b8046aaa0242c27
inside <inside@inside.com>
2015-12-31 23:11:58 +0200

    Add JsonEncoder class

---
+++ app/Helper/JsonEncoder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Storage\Helper;
+
+class JsonEncoder
+{
+    public static function createResponse($text, $error = null)
+    {
+        return self::encode(["text" => $text, "error" => $error]);
+    }
+
+    public static function encode($item)
+    {
+        return json_encode($item);
+    }
+
+    public static function decode($item)
+    {
+        return json_decode($item);
+    }
+}

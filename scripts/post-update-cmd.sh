1cf91fce555dd7cbcebbd81fad5e035aa85b86a8
inside <inside@inside.com>
2016-01-02 20:29:58 +0200

    Add bootstrap, jquery, CustomFileInputs lib; script for copying assets to web folder

---
+++ scripts/post-update-cmd.sh
@@ -0,0 +1,11 @@
+cp vendor/twbs/bootstrap/dist/css/bootstrap.min.css web/vendor-css/bootstrap.min.css;
+cp vendor/twbs/bootstrap/dist/js/bootstrap.min.js web/vendor-js/bootstrap.min.js;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.eot web/fonts/glyphicons-halflings-regular.eot;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.svg web/fonts/glyphicons-halflings-regular.svg;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf web/fonts/glyphicons-halflings-regular.ttf;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff web/fonts/glyphicons-halflings-regular.woff;
+cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2 web/fonts/glyphicons-halflings-regular.woff2;
+cp vendor/components/jquery/jquery.min.js web/vendor-js/jquery.min.js;
+cp vendor/codepo8/CustomFileInputs/css/component.css web/vendor-css/component.css;
+cp vendor/codepo8/CustomFileInputs/css/normalize.css web/vendor-css/normalize.css;
+cp vendor/codepo8/CustomFileInputs/js/custom-file-input.js web/vendor-js/custom-file-input.js;

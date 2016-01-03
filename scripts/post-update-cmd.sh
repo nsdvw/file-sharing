dfd8ae8743c728bf544e8222736ed74b088903b2
inside <inside@inside.com>
2016-01-03 15:09:09 +0200

    publish jplayer assets

--- scripts/post-update-cmd.sh
+++ scripts/post-update-cmd.sh
@@ -5,7 +5,16 @@ cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.svg web/fonts/g
 cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf web/fonts/glyphicons-halflings-regular.ttf;
 cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff web/fonts/glyphicons-halflings-regular.woff;
 cp vendor/twbs/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2 web/fonts/glyphicons-halflings-regular.woff2;
+
 cp vendor/components/jquery/jquery.min.js web/vendor-js/jquery.min.js;
+
 cp vendor/codepo8/CustomFileInputs/css/component.css web/vendor-css/component.css;
 cp vendor/codepo8/CustomFileInputs/css/normalize.css web/vendor-css/normalize.css;
 cp vendor/codepo8/CustomFileInputs/js/custom-file-input.js web/vendor-js/custom-file-input.js;
+
+cp vendor/happyworm/jplayer/dist/jplayer/jquery.jplayer.min.js web/vendor-js/jquery.jplayer.min.js;
+cp vendor/happyworm/jplayer/dist/jplayer/jquery.jplayer.swf web/vendor-js/jquery.jplayer.swf;
+cp vendor/happyworm/jplayer/dist/skin/blue.monday/css/jplayer.blue.monday.min.css web/vendor-css/jplayer.blue.monday.min.css;
+cp vendor/happyworm/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.jpg web/image/jplayer.blue.monday.jpg;
+cp vendor/happyworm/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.seeking.gif web/image/jplayer.blue.monday.seeking.gif;
+cp vendor/happyworm/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.video.play.png web/image/jplayer.blue.monday.video.play.png;

b754e7b44a337b36750a55d8eb15e1eebdab7f5f
inside <inside@inside.com>
2016-01-04 16:20:06 +0200

    Ajax comments and form js

---
+++ web/js/comment-ajax.js
@@ -0,0 +1,48 @@
+$(function () {
+    $("#commentForm").on("submit", function (event) {
+        event.preventDefault();
+        var form = document.forms.comment_form;
+        var formData = new FormData(form);
+        var replyID = form.querySelector(".replyID").value;
+        var errorBox = $("#commentForm .text-danger");
+        errorBox.text("");
+        $.ajax({
+            "method": "POST",
+            "url": "/view/" + window.location.pathname.split("/").pop() +
+                "?ajax&reply=" + replyID,
+            "data": formData,
+            "dataType": "json",
+            "processData": false,
+            "contentType": false
+        }).done(function (response) {
+            if (response.error) {
+                errorBox.text(response.error);
+            } else {
+                var comment = response.text.comment;
+                var author = response.text.author;
+                appendComment("#commentTemplate", comment, author);
+                form.reset();
+            }
+        }).fail(function (response) {
+            errorBox.text("Server error " + response.status + ", please try again later");
+        });
+    });
+});
+
+function appendComment(templateSelector, comment, author) {
+    var template = $(templateSelector).html();
+    var indexOfLevel = template.indexOf("{level}");
+    template = template.substring(0, indexOfLevel)
+            + comment.level
+            + template.substring(indexOfLevel + 7);
+    template = $(template);
+    author = author || 'Anonymous';
+    $(".media-heading", template).text(author);
+    $(".comment-text", template).text(comment.contents);
+    $(".added", template).text(comment.added);
+    $(".reply", template)
+        .attr("href", "/view/" + comment.file_id + "?reply=" + comment.id)
+        .attr("data-reply-id", comment.id)
+        .html("#" + comment.id + " Reply");
+    $("#comments").append(template);
+}
---
+++ web/js/comment-form.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    $(".reply").on("click", function (event) {
+        event.preventDefault();
+        var replyID = $(this).data("reply-id");
+        $("#commentForm .replyID").attr("value", replyID);
+        $("#commentForm").insertAfter(
+            $(".media:has(a[data-reply-id=" + replyID + "])")
+        );
+        var replyButton = $("#reply");
+        replyButton.attr("data-reply-id", replyID).removeClass("invisible");
+        $(".reply-container", replyButton).text("Reply to #" + replyID);
+    });
+
+    $("#commentForm .glyphicon-remove").on("click", function (event) {
+        $("#commentForm").prependTo("#comments");
+        $("#commentForm .replyID").attr("value", "");
+        var replyButton = $("#reply");
+        replyButton.addClass("invisible");
+        //$(".reply-container", replyButton).text("");
+    });
+
+});

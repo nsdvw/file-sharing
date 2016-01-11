426e9d62ef8a06e98d0a5432b41c05ec8c960544
inside <inside@inside.com>
2016-01-11 23:05:25 +0200

    Adjust comment form script

--- web/js/comment-form.js
+++ web/js/comment-form.js
@@ -1,22 +1,21 @@
 $(function () {
+    var commentForm = $("#commentForm");
+    var replyButton = $("#reply");
 
-    $(".reply").on("click", function (event) {
+    $("#comments").on("click", ".reply", function (event) {
         event.preventDefault();
         var replyID = $(this).data("reply-id");
-        $("#commentForm .replyID").attr("value", replyID);
-        $("#commentForm").insertAfter(
+        $(".replyID", commentForm).attr("value", replyID);
+        commentForm.insertAfter(
             $(".media:has(a[data-reply-id=" + replyID + "])")
         );
-        var replyButton = $("#reply");
         replyButton.attr("data-reply-id", replyID).removeClass("hidden");
         $(".reply-container", replyButton).text("Reply to #" + replyID);
     });
 
-    $("#commentForm .glyphicon-remove").on("click", function (event) {
-        $("#commentForm").prependTo("#comments");
-        $("#commentForm .replyID").attr("value", "");
-        var replyButton = $("#reply");
+    $(".glyphicon-remove", commentForm).on("click", function (event) {
+        commentForm.prependTo("#comments");
+        $(".replyID", commentForm).attr("value", "");
         replyButton.addClass("hidden");
     });
-
 });
